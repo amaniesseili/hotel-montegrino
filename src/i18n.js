2da03bd925c4f924759e6649207c5a543413b32f
amani esseili <esseiliamani@hotmail.com>
2024-01-11 10:58:14 +0100

    Set up vue-i18n for multilingual support

---
+++ src/i18n.js
@@ -0,0 +1,46 @@
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'it', // Lingua predefinita
+  messages: {
+    //------------------------------------------------
+    it: {
+      // le traduzioni italiane - Header
+      home: 'Home',
+      chiSiamo: 'Chi Siamo',
+      galleria: 'Galleria',
+      recensioni: 'Recensioni',
+      servizi: 'Servizi',
+      contatti: 'Contatti',
+      prenota: 'Prenota',
+      lingua: 'Lingua',
+      
+      // traduzioni per le altre pagine
+      // traduzioni per la Homepage
+    },
+    //------------------------------------------------
+    en: {
+      // le traduzioni inglesi - Header
+      home: 'Home',
+      chiSiamo: 'About Us',
+      galleria: 'Galley',
+      recensioni: 'Reviews',
+      servizi: 'Services',
+      contatti: 'Contact us',
+      prenota: 'Book',
+      lingua: 'Language',
+      // traduzioni per le altre pagine
+      // traduzioni per la Homepage
+    },
+    //----------------------------------------------
+    // le traduzioni francese - Header
+
+    //----------------------------------------------
+    // le traduzioni tedesco - Header
+
+    //----------------------------------------------
+  },
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import './style/general.scss'
 import App from './App.vue'
+import i18n from './i18n'
+
+
+const app = createApp(App);
+app.use(i18n);
+
 
 // Utilizzo del router nel main file (main.js)
 import { router } from "./router";
