a6460b53d23b4dbf0e06950285123c962970130f
amani esseili <esseiliamani@hotmail.com>
2024-01-18 14:38:30 +0100

    update files Json

--- src/pages/Contatti.vue
+++ src/pages/Contatti.vue
@@ -30,30 +30,29 @@ export default {
     <div class="row">
       <div class="col-12 col-md-12 col-lg-6">
 
-        <TomTom></TomTom>
+        <!-- <TomTom></TomTom> -->
         <!-- Mappa -->
         <!--  il tuo codice per la mappa  -->
       </div>
       <div class="col-12 col-md-12 col-lg-6">
 
         <div class="contact-us">
-          <h2 class="mt-1">Entra in Contatto con Noi</h2>
-          <p class="me-5">Per assistenza, ti invitiamo a contattarci tramite telfono, inviarci e-mail o visitare il nostro forum.
-            Garantiamo una pronta risposta alla tua richiesta.</p>
+          <h2 class="mt-1">{{ $t ('contactsTitle')}}</h2>
+          <p class="me-5">{{ $t ('contactsDescription')}}</p>
           <!-- phone,address,email -->
           <div class="row">
 
             <div class="col-12 col-md-4 mb-1">
               <div class="contact-column h-100">
                 <i class="fas fa-phone"></i>
-                <h5>Telefono</h5>
+                <h5>{{ $t ('contact.phone')}}</h5>
                 <p>0039 03321503155</p>
               </div>
             </div>
             <div class="col-12 col-md-4 mb-1">
               <div class="contact-column h-100">
                 <i class="fas fa-map-marker-alt "></i>
-                <h5>Indirizzo</h5>
+                <h5>{{ $t ('contact.address') }}</h5>
                 <p>Via Cadorna, 26, 21010, <br> Montegrino Valtravaglia VA</p>
               </div>
             </div>
@@ -61,7 +60,7 @@ export default {
             <div class="col-12 col-md-4 mb-1">
               <div class="contact-column h-100">
                 <i class="fas fa-envelope"></i>
-                <h5>Email</h5>
+                <h5>{{ $t ('contact.email') }}</h5>
                 <p>info@hotelmontegrino.it</p>
               </div>
             </div>
